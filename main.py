36cd908dfe2e72ee24b8bd80baaf701bab14616a
LordJashin32 <tristanlangford32@gmail.com>
2020-02-13 18:05:34 -0500

    update

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-#!/usr/bin/env
-# Made by LordJashin32
+#!/usr/bin/python3
 
 from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
+chatbot = ChatBot(
+    "My ChatterBot",
+    output_adapter="chatterbot.output.Gitter",
+    gitter_api_token="my-gitter-api-token",
+    gitter_room="my-room-name",
+    gitter_only_respond_to_mentions=False,
+)
+print('Hello World')
+
+
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+trainer.train(
+    "chatterbot.corpus.english"
+)
