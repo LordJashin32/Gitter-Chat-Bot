bb9a558e1e8126003a9686e0363e71c9a2e65306
LordJashin32 <tristanlangford32@gmail.com>
2020-02-13 22:35:05 -0500

    final

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import UbuntuCorpusTrainer
+from chatterbot.conversation import Statement
 
 chatbot = ChatBot(
-    "My ChatterBot",
+    "My ChatterBot", # Name of your bot
+    input_adapter="chatterbot.input.Gitter",
     output_adapter="chatterbot.output.Gitter",
-    gitter_api_token="my-gitter-api-token",
-    gitter_room="my-room-name",
-    gitter_only_respond_to_mentions=False,
+    gitter_api_token="my-gitter-api-token", # Gitter API token
+    gitter_room="my-room-name", # Gitter Room Name
+    gitter_only_respond_to_mentions=False, # Bot responds to all messages in room
+    preprocessors=[
+        'chatterbot.preprocessors.clean_whitespace'
+    ],
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch'    ]
 )
-print('Hello World')
 
+# 500mb but gives us very good responses to start with
+trainer = UbuntuCorpusTrainer(chatbot)
 
-trainer = ChatterBotCorpusTrainer(chatbot)
+# Enter your own custom stuff here
+trainer1 = ListTrainer(chatbot)
+trainer1.train([
+    "Input question user asks on gitter?",
+    "Response that the bot will give to question on gitter above.",
+])
 
-trainer.train(
-    "chatterbot.corpus.english"
-)
+# Start by training our bot with the Ubuntu corpus data
+trainer.train()
+
+while True:
+    try:
+        response = chatbot.get_response(None)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
