01ab728cbb95a5fe2295b32aac716dfada7d0fb8
LordJashin32 <tristanlangford32@gmail.com>
2020-02-14 17:50:11 -0500

    major updates

--- README.md
+++ README.md
@@ -10,15 +10,17 @@ and the appropriate packages there.
 ```bash
 # use python or pypy
 sudo apt install pypy3 python3 python3-dev python3-pip
-pip3 install setuptools wheel chatterbot chatterbot_corpus
+pip3 install setuptools wheel chatterbot chatterbot_corpus gitterpy
 git clone http://github.com/LordJashin32/Gitter-Chat-Bot
 cd Gitter-Chat-Bot
 python3 ./main.py
 
 ```
 Make sure to fill in your Gitter API Key, and Room ID for
-your usage. 
+your usage inside main.py. 
 
 ## Usage
 
 This can be used for a variety of purposes but remember that nothing trumps real human interaction. So if you are trying to get people for something it might be better to handle that yourself than to use a bot.
+
+This replies to any messages in the room period with @Username and the response except for messages from itself it will not respond to. 
--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import UbuntuCorpusTrainer
-from chatterbot.conversation import Statement
+from gitterpy.client import GitterClient
+import json
 
 chatbot = ChatBot(
-    "My ChatterBot", # Name of your bot
-    input_adapter="chatterbot.input.Gitter",
-    output_adapter="chatterbot.output.Gitter",
-    gitter_api_token="my-gitter-api-token", # Gitter API token
-    gitter_room="my-room-name", # Gitter Room Name
-    gitter_only_respond_to_mentions=False, # Bot responds to all messages in room
+    "YOUR_BOT_NAME", # Name of your bot
     preprocessors=[
         'chatterbot.preprocessors.clean_whitespace'
     ],
@@ -22,23 +16,26 @@ chatbot = ChatBot(
         'chatterbot.logic.BestMatch'    ]
 )
 
-# 500mb but gives us very good responses to start with
-trainer = UbuntuCorpusTrainer(chatbot)
+# Run this once to generate good stuff for database to start with
+# Warning takes 6 hours approximately to run
+#trainer = UbuntuCorpusTrainer(chatbot)
+#trainer.train()
 
-# Enter your own custom stuff here
-trainer1 = ListTrainer(chatbot)
-trainer1.train([
-    "Input question user asks on gitter?",
-    "Response that the bot will give to question on gitter above.",
-])
+gitter_api_token='YOUR_API_KEY' # Gitter API token
+gitter_room='YOUR_ROOM/YOUR_SUB_ROOM' # Gitter Room Name
 
-# Start by training our bot with the Ubuntu corpus data
-trainer.train()
-
+gitter = GitterClient(gitter_api_token)
 while True:
     try:
-        response = chatbot.get_response(None)
-
+        response = gitter.stream.chat_messages(gitter_room)
+        for stream_messages in response.iter_lines():
+            if stream_messages != "":
+                resp = json.loads(stream_messages)
+                msg = resp['text']
+                userid = resp['fromUser']['username']
+                if userid == gitter.auth.get_my_id['name']:
+                    continue
+                gitter.messages.send(gitter_room, "@" + userid + " " + chatbot.get_response(msg))
     # Press ctrl-c or ctrl-d on the keyboard to exit
     except (KeyboardInterrupt, EOFError, SystemExit):
         break
